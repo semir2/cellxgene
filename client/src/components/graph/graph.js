febef85342d4cb1af74d908899134d3e9ece0dc4
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2019-08-02 12:02:37 -0700

    persist uncalc coordinates

--- client/src/components/graph/graph.js
+++ client/src/components/graph/graph.js
@@ -289,7 +289,9 @@ class Graph extends React.Component {
       stateChanges = { ...stateChanges, toolSVG: newToolSVG, tool, container };
     };
 
-    const createCentroidSVG = () => {
+    const createCentroidSVG = (
+      viewportChange = true /* this should be defaulted to false */
+    ) => {
       d3.select("#graphAttachPoint")
         .select("#centroid-container")
         .remove();
@@ -307,9 +309,14 @@ class Graph extends React.Component {
       let value;
       while (pair) {
         value = pair[1];
-        if (!value[2]) {
-          value.splice(0, 2, ...this.mapPointToScreen([value[0], value[1]]));
+        if (!value[2] || viewportChange) {
+          console.log("before:", value);
+
           value[2] = true;
+
+          value.splice(3, 2, ...this.mapPointToScreen([value[0], value[1]]));
+
+          console.log(value);
         }
         pair = iter.next().value;
       }
@@ -330,7 +337,7 @@ class Graph extends React.Component {
     ) {
       // If the window size has changed we want to recreate all SVGs
       createToolSVG();
-      createCentroidSVG();
+      createCentroidSVG(true);
     } else if (
       (responsive.height && responsive.width && !toolSVG) ||
       selectionTool !== prevProps.selectionTool
