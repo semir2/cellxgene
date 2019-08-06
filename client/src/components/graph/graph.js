25f16319c0228d31277bd4329a03366e5a803092
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2019-08-06 13:29:01 -0700

    remove special characters from svg id

--- client/src/components/graph/graph.js
+++ client/src/components/graph/graph.js
@@ -388,7 +388,9 @@ class Graph extends React.Component {
 
     if (pointDilation.categoryField) {
       svg
-        ?.select(`#svg${pointDilation.categoryField}-label`)
+        ?.select(
+          `#svg${pointDilation.categoryField.replace(/[^\w\s]/gi, "")}-label`
+        )
         .style("font-size", "18px")
         .style("font-weight", "800");
     }
